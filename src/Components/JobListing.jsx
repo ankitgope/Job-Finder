d041df167efc559d8310dc2b4e047234072e829b
Ankit Gope <ankitgopetatan@gmail.com>
2024-06-11 21:59:17 +0530

    Second Commit - Data fecthig process changed to async process

--- src/Components/JobListing.jsx
+++ src/Components/JobListing.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-import data from "../jobs.json"
+import { useEffect,useState } from "react";
 import JobListingCard from "./JobListingCard";
 const JobListing = ({isHome=false}) => {
+
+  const[jobs,setJobs] = useState([])
+  const[loading,setLoading] = useState(true)
+  useEffect(()=>{
+    const fetchJobs = async()=>{
+      try {
+        // forlimit of 3 card ("http://localhost:8000/jobs?_limit=3")
+        const res = await fetch('http://localhost:8000/jobs')
+        const data= await res.json();
+        setJobs(data)
+        
+      } catch (error) {
+        console.log('No data found')
+      }
+      finally{
+        setLoading(false)
+      }
+    }
+    fetchJobs()
+  },[])
   
-  const { jobs } = data;
-  const jobsListings = isHome? jobs.slice(0,3): jobs
+  
+  
   return (
     <section className="bg-blue-50 px-4 py-3 overflow-auto">
       <div className="container-xl lg:container m-auto">
@@ -12,10 +32,13 @@ const JobListing = ({isHome=false}) => {
         {isHome?'Browse Jobs' :'Recent Jobs'}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {
-            jobsListings.map((job)=>(
-              <JobListingCard key={job.id} job={job}/>
-            ))
+          {loading? (<h2>loading</h2>):(
+            <>
+              {jobs.map((job)=>(
+                  <JobListingCard key={job.id} job={job}/>
+                ))
+              }
+            </>)
           }
         </div>
       </div>
